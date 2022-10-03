2e7acea7372263dd4bebd642653aa83d92730704
Md. Sakil Islam <sakilislam1080@gmail.com>
2022-10-03 15:44:43 +0600

    design initialize

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./components/About/About";
+import AllInOne from "./components/AllInOne/AllInOne";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Pricing from "./components/Pricing/Pricing";
+import Support from "./components/Support/Support";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Header />
+      <About />
+      <Support />
+      <AllInOne />
+      <Pricing />
+      <Footer />
+    </>
   );
 }
 
